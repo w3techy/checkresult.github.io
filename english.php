d613d8da9a201753206fa51d5f60f2e1a8345a3f
W3Techy <webmaster@w3techy.com>
2018-05-11 07:44:17 +0100

    Add files via upload

---
+++ english.php
@@ -0,0 +1,19 @@
+<?php
+$sql = "SELECT * FROM english WHERE ID='$user_check'";
+$query = mysqli_query($conn, $sql);
+if (!$query) {
+   die ('SQL Error: ' . mysqli_error($conn));
+  }
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query))
+		{
+			$score  = $row['score'] == 0 ? '' : number_format($row['score']);
+			echo '<tr>
+					<td>English</td>
+					<td>'.$score.'</td>
+					<td>'.$row['grade'].'</td>
+					<td>'.$row['remarks'].'</td>
+					
+				</tr>';
+		}?>
